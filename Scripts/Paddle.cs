4371488a50e3f77b15723bbca2c4d330af5b663a
MH-Shamim <36614742+MH-Shamim@users.noreply.github.com>
2018-02-19 10:54:54 +0600

    Add files via upload

---
+++ Scripts/Paddle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour 
+{
+	public float paddleSpeed = 1F;
+	public Vector3 playerPos = new Vector3(0,0,0);
+
+	void Update()
+	{
+		float yPos = gameObject.transform.position.y + (Input.GetAxis("Vertical") * paddleSpeed);
+		playerPos = new Vector3(-20, Mathf.Clamp(yPos, -11, 11), 0);
+		gameObject.transform.position = playerPos;
+	}
+}
